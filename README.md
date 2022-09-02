7c1504d00a07b8fd7d89c969303a5cc61cf0a0df
Songkeys <songv587@gmail.com>
2022-09-02 18:01:15 +0800

    feat: add sitemap generator

---
+++ README.md
@@ -0,0 +1,1 @@
+# Crossbell.io Sitemap Auto Generator
---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { request } = require("undici");
+const { simpleSitemapAndIndex } = require("sitemap");
+const path = require("path");
+
+async function fetchCharacters({ cursor = 1, limit = 1000 }) {
+	const { body } = await request("https://indexer.crossbell.io/v1/graphql", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: `{"query":"{ characters(cursor: { characterId: ${cursor} }, take: ${limit}) { handle } }"}`,
+	});
+
+	const res = await body.json();
+
+	return {
+		characters: res.data.characters,
+		cursor: cursor + limit - 1,
+	};
+}
+
+async function main() {
+	const limit = 1000;
+	let cursor = 1;
+	let handleUrls = [];
+	while (true) {
+		const res = await fetchCharacters({ cursor, limit });
+		handleUrls = handleUrls.concat(res.characters.map((c) => `/@${c.handle}`));
+		cursor = res.cursor;
+		if (res.characters.length < limit) {
+			break;
+		}
+	}
+
+	simpleSitemapAndIndex({
+		hostname: "https://crossbell.io",
+		destinationDir: path.resolve(__dirname, "..", "sitemaps"),
+		sourceData: handleUrls,
+		publicBasePath: "/sitemaps",
+		gzip: false,
+	});
+}
+
+main().catch((e) => console.error(e));
